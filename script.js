af42c4a314a895835b1bedf3938e0f93abf73754
luis Milani <luismilani02@gmail.com>
2025-03-21 23:39:56 -0300

    javascript novo + semantica html

--- script.js
+++ script.js
@@ -1,36 +1,126 @@
 let horas = document.getElementById('horas')
 let minutos = document.getElementById('minutos')
 let segundos = document.getElementById('segundos')
+let parar = document.getElementById('parar')
+let resetar = document.getElementById('resetar')
+let titulo = document.getElementById('titulo')
+let horaInicial, minutoInicial, segundoInicial
 let rodando = false
-let tempoTotal = 0
+let tempoTotal = 0;
+let tempo
 
-function comecar() {
-    if (!rodando) {
-        rodando = true
-        rodar()
+parar.addEventListener('click', para)
+resetar.addEventListener('click', reseta)
+
+function values() {
+    let horaAtual = Number(horas.value) || 0
+    let minutoAtual = Number(minutos.value) || 0
+    let segundoAtual = Number(segundos.value) || 0
+
+    return {horaAtual, minutoAtual, segundoAtual}
+}
+
+function comeca() {    
+    let { horaAtual, minutoAtual, segundoAtual } = values();
+
+    horaInicial = horaAtual;
+    minutoInicial = minutoAtual;
+    segundoInicial = segundoAtual;
+
+    horaAtual = horaAtual * 60 * 60;
+    minutoAtual = minutoAtual * 60;
+    tempoTotal = horaAtual+minutoAtual+segundoAtual;
+
+    if (tempoTotal <= 0) {
+        invalido()
+    } else {
+        console.log(`tempo total: ${tempoTotal}`)
+
+        tempo = setInterval(() => {
+            tempoTotal--;
+            console.log(tempoTotal)
+            temp()
+            if (tempoTotal <= 0) {
+                clearInterval(tempo)
+            }
+        }, 1000);
     }
 }
 
-function rodar() {
-    if (rodando == true) {
-        console.log('rodando ' + tempoTotal)
-        tempoTotal = tempoTotal - 1
-        rodando = false
+function temp() {
+    let horasCon = Math.floor(tempoTotal / 3600)
+    let minutosCon = Math.floor((tempoTotal % 3600) / 60)
+    let segundosCon = Math.floor(tempoTotal % 60)
+
+    console.log(`teste ${horasCon}, ${minutosCon}, ${segundosCon}`)
+
+    if (horasCon <= 9) {
+        horas.value = '0' + horasCon
+    } else {
+        horas.value = horasCon
+    }
+     
+    if (minutosCon <= 9) {
+        minutos.value = '0' + minutosCon
+    } else {
+        minutos.value = minutosCon
+    }
+
+    if (segundosCon <= 9) {
+        segundos.value = '0' + segundosCon
+    } else {
+        segundos.value = segundosCon
     }
 }
 
-function calculaTempo() {
-    let horaAtual = +horas.value
-    let minutoAtual = +minutos.value
-    let segundoAtual = +segundos.value
+function para() {
+    let { horaAtual, minutoAtual, segundoAtual } = values();
 
-    // console.log(horaAtual + minutoAtual + segundoAtual)
+    clearInterval(tempo)
 
-    horaAtual = horaAtual * 60 * 60 
-    // console.log(horaAtual)
-    minutoAtual = minutoAtual * 60
-    // console.log(minutoAtual)
-    tempoTotal += segundoAtual + horaAtual + minutoAtual
+    if (segundoAtual <= 9) {
+        segundos.value = '0' + segundoAtual
+    } else {
+        segundos.value = segundoAtual
+    }
 
-    console.log(tempoTotal)
+    if (minutoAtual <= 9) {
+        minutos.value = '0' + minutoAtual
+    } else {
+        minutos.value = minutoAtual
+    }
+
+    if (horaAtual <= 9) {
+        horas.value = '0' + horaAtual
+    } else {
+        horas.value = horaAtual
+    }
+}
+
+function reseta() {
+    clearInterval(tempo)
+
+    if (horaInicial <= 9) {
+        horas.value = '0' + horaInicial
+    } else {
+        horas.value = horaInicial
+    }
+     
+    if (minutoInicial <= 9) {
+        minutos.value = '0' + minutoInicial
+    } else {
+        minutos.value = minutoInicial
+    }
+
+    if (segundoInicial <= 9) {
+        segundos.value = '0' + segundoInicial
+    } else {
+        segundos.value = segundoInicial
+    }
+}
+
+function invalido() {
+    let invaTxt = document.getElementById('invalido')
+    
+    
 }
